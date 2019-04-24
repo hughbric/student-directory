e5076417687c258f0011eba81022159222508b36
Michael <mhughes36@live.co.uk>
2019-04-24 22:03:20 +0000

    Added an interactive menu

--- directory.rb
+++ directory.rb
@@ -6,19 +6,19 @@ class Directory
   end
   
   def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+    puts "The students of Villains Academy".center(50)
+    puts "-------------".center(50)
   end
   
   def print_footer
-    puts "Overall, we have #{students.length} great students"
+    puts "-------------".center(50)
+    puts "Overall, we have #{students.length} great students".center(50)
   end
   
   def print_students
     if students.length > 0
       students.each_with_index do |student, index|
-        print "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-        puts " | Age: #{student[:age]}, #{student[:place]}"
+        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) | Age: #{student[:age]}, #{student[:place]}".center(50)
       end
     end
   end
@@ -33,7 +33,6 @@ class Directory
       
       puts "Please enter their cohort:"
       cohort = gets.chomp
-      cohort = "April" if cohort.empty?
       
       puts "Their place of birth:"
       place = gets.chomp
@@ -41,20 +40,47 @@ class Directory
       puts "Their age:"
       age = gets.chomp
       
-      students << { name: name, cohort: cohort.to_s, place: place, age: age }
+      assign_values(name, cohort, place, age)
+    end
+  end
+  
+  def assign_values(name, cohort, place, age)
+    cohort = "April"    if cohort.empty?
+    place = "La Mancha" if place.empty?
+    age = "48"          if age.empty?
+    
+    students << { name: name, cohort: cohort.intern, place: place, age: age }
 
-      if students.length <= 1
-        puts "We now have #{students.length} student"
+    if students.length <= 1
+      puts "We now have #{students.length} student"
+    else
+      puts "Now we have #{students.length} students"  
+    end
+  end
+  
+  def interactive_menu
+    loop do
+      puts "1. Input new students"
+      puts "2. Show our current students"
+      puts "9. Exit"
+      
+      selection = gets.chomp
+      
+      case selection
+      when "1"
+        input_students
+      when "2"
+        print_header
+        print_students
+      when "9"
+        print_footer
+        break
       else
-        puts "Now we have #{students.length} students"
+        puts "I don't know what you meant, try again"
       end
     end
   end
 end
 
 students = Directory.new
-
-students.input_students
-students.print_header
-students.print_students
-students.print_footer
+students.interactive_menu
